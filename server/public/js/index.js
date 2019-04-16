09ce40275479c59897ef5196d509f8d5857e85bf
han yu <1807003141@qq.com>
2019-04-16 21:56:57 +0800

    编写定时模块.

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -93,7 +93,66 @@
 /* 4 */
 /***/ (function(module, exports) {
 
-	"use strict";
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	/*
+	 * 定时器模块
+	 */
+
+	var Timer = function () {
+	  function Timer() {
+	    _classCallCheck(this, Timer);
+	  }
+
+	  _createClass(Timer, [{
+	    key: 'countDown',
+	    value: function countDown(end, update, handle) {
+	      var now = new Date().getTime();
+	      var self = this;
+
+	      if (now - end) {
+	        handle.call(self);
+	      } else {
+	        var d_to_ms = 1000 * 60 * 60 * 24; // 一天的毫秒数
+	        var h_to_ms = 1000 * 60 * 60; // 一小时的毫秒数
+	        var m_to_ms = 1000 * 60; // 一分钟的毫秒数
+	        var s_to_ms = 1000; // 一秒的毫秒数
+
+	        var last_time = end - now; // 时间差
+
+	        var d = Math.floor(last_time / d_to_ms);
+	        var h = Math.floor((last_time - d * d_to_ms) / h_to_ms);
+	        var m = Math.floor((last_time - d * d_to_ms - h * h_to_ms) / m_to_ms);
+	        var s = Math.floor((last_time - d * d_to_ms - h * h_to_ms - m * m_to_ms) / s_to_ms);
+
+	        var arr = [];
+	        if (d > 0) arr.push(d + '\u5929');
+	        if (h > 0 || arr.length > 0) arr.push(h + '\u5C0F\u65F6');
+	        if (m > 0 || arr.length > 0) arr.push(m + '\u5206\u949F');
+	        if (s > 0 || arr.length > 0) arr.push(s + '\u79D2');
+
+	        self.last_time = arr.join('');
+	        update.call(self, arr.join(''));
+
+	        setTimeout(function () {
+	          self.countDown(end, update, handle);
+	        }, 1000);
+	      }
+	    }
+	  }]);
+
+	  return Timer;
+	}();
+
+	exports.default = Timer;
 
 /***/ }),
 /* 5 */
---
+++ src/js/timer.js
@@ -0,0 +1,39 @@
+/*
+ * 定时器模块
+ */
+
+export default class Timer {
+  countDown(end, update, handle) {
+    const now = new Date().getTime();
+    const self = this;
+
+    if (now - end) {
+      handle.call(self);
+    } else {
+      const d_to_ms = 1000 * 60 * 60 * 24; // 一天的毫秒数
+      const h_to_ms = 1000 * 60 * 60; // 一小时的毫秒数
+      const m_to_ms = 1000 * 60; // 一分钟的毫秒数
+      const s_to_ms = 1000; // 一秒的毫秒数
+          
+      let last_time = end - now; // 时间差
+
+      let d = Math.floor(last_time / d_to_ms);
+      let h = Math.floor((last_time - d * d_to_ms) / h_to_ms);
+      let m = Math.floor((last_time - d * d_to_ms - h * h_to_ms) / m_to_ms);
+      let s = Math.floor((last_time - d * d_to_ms - h * h_to_ms - m * m_to_ms) / s_to_ms);
+
+      let arr = [];
+      if (d > 0) arr.push(`${d}天`);
+      if (h > 0 || arr.length > 0) arr.push(`${h}小时`);
+      if (m > 0 || arr.length > 0) arr.push(`${m}分钟`);
+      if (s > 0 || arr.length > 0) arr.push(`${s}秒`);
+
+      self.last_time = arr.join('');
+      update.call(self, arr.join(''));
+
+      setTimeout(function () {
+          self.countDown(end, update, handle);
+      }, 1000);
+    }
+  }
+}
